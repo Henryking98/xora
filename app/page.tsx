ddcaccc933a6d5baef4fb905f608b7a319d8a4e6
Henry <94872110+Henryking98@users.noreply.github.com>
2025-01-13 02:49:33 +0100

    added testimonial section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Features from "@/components/Features";
 import Pricing from "@/components/Pricing";
+import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <Hero/>
         <Features/>
         <Pricing/>
+        <Testimonials/>
     </main>
   );
 }
---
+++ components/Testimonials.tsx
@@ -0,0 +1,82 @@
+import { testimonials } from "@/constants";
+import clsx from "clsx";
+import Image from "next/image";
+
+interface Testimonial {
+  id: string;
+  avatarUrl: string;
+  name: string;
+  role: string;
+  comment: string;
+}
+
+interface TestimonialItemProps {
+  item: Testimonial;
+  containerClassName?: string;
+}
+const TestimonialItem = ({ item, containerClassName }: TestimonialItemProps) => {
+  return (
+    <div
+      className={clsx(
+        "relative px-14 pb-14 pt-11 after:absolute after:bottom-0 after:right-0 after:h-0.5 after:w-screen after:bg-s2 after:content-[''] max-md:px-0 max-md:pt-11 after:max-md:-right-4",
+        containerClassName,
+      )}
+    >
+      <blockquote className="h6 mb-8 text-p4">{item.comment}</blockquote>
+
+      <div className="flex items-center max-xl:-mr-8">
+        <div className="mr-4 size-20 shrink-0 rounded-half border-2 border-s2 p-1.5 relative">
+          <Image
+            src={item.avatarUrl}
+            alt={item.name}
+            className="size-full object-cover"
+            fill
+          />
+        </div>
+        <div>
+          <h4 className="body-2 mb-0.5 text-p1">{item.name}</h4>
+          <p className="small-compact uppercase text-s3">{item.role}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Testimonials = () => {
+  const halfLength = Math.floor(testimonials.length / 2);
+
+  return (
+    <section className="relative z-2 py-24 md:py-28 lg:py-40">
+      <div className="container block lg:flex">
+        <div className="testimonials_head-res relative z-2 mr-20 flex-300">
+          <p className="caption mb-5 max-md:mb-2.5">Wall of Love</p>
+          <h3 className="h3 max-md:h5 text-p4">Words from our fans</h3>
+        </div>
+
+        <div className="testimonials_inner-after testimonials_inner-before relative -my-12 -mr-3 flex items-start max-lg:static max-md:block">
+          <div className="testimonials_group-after flex-50">
+            {testimonials.slice(0, halfLength).map((testimonial) => (
+              <TestimonialItem
+                key={testimonial.id}
+                item={testimonial}
+                containerClassName="last:after:hidden last:after:max-md:block"
+              />
+            ))}
+          </div>
+
+          <div className="flex-50">
+            {testimonials.slice(halfLength).map((testimonial) => (
+              <TestimonialItem
+                key={testimonial.id}
+                item={testimonial}
+                containerClassName="last:after:hidden after:right-auto after:left-0 after:max-md:-left-4 md:px-12"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
